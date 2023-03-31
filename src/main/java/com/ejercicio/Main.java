bfd571db5a2235b93dc4d863e7fa1b845f4b90e9
Sebas Villamizar <sebasvillamizar@MacBook-Pro-de-Sebas.local>
2023-03-31 17:13:52 -0500

    first commit

---
+++ src/main/java/com/ejercicio/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ejercicio;
+
+/**
+ *
+ * @author sebasvillamizar
+ */
+public class Ejercicio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
